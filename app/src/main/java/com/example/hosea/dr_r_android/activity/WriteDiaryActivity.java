94f69b9e136947d82b8ade6b8c8f38dccc7ff7b8
mslee1002 <leemsmail@naver.com>
2016-11-24 01:48:00 +0900

    TTS , STT 기능 추가

--- app/src/main/java/com/example/hosea/dr_r_android/activity/WriteDiaryActivity.java
+++ app/src/main/java/com/example/hosea/dr_r_android/activity/WriteDiaryActivity.java
@@ -2,6 +2,9 @@ package com.example.hosea.dr_r_android.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidquery.AQuery;
@@ -20,73 +24,90 @@ import com.example.hosea.dr_r_android.R;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class WriteDiaryActivity extends AppCompatActivity {
+public class WriteDiaryActivity extends AppCompatActivity  {
 
     private Intent previousIntent;
     private AQuery aq = new AQuery(this);
     RadioButton drinking;
     Button submit;
     Object b_menu1,b_menu2,b_menu3;
+    Button recordAudio;
+    TextView audioToText;
     Object l_menu1,l_menu2,l_menu3;
     Object d_menu1,d_menu2,d_menu3;
     Object temperature ,humidity ,sleepTime, bloodPressure ;
-
+    SpeechRecognizer mRecognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writediary);
+
+
         previousIntent = getIntent();
+        recordAudio = (Button)findViewById(R.id.audio);
+        audioToText = (TextView)findViewById(R.id.breakfast);
+
+        recordAudio.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent= new Intent(getApplicationContext(),VoiceToTextActivity.class);
+                intent.putExtra("tospeach", "아침 머드셧나요");
+                startActivityForResult(intent,1001);
+            }
+        });
+
 
         drinking = (RadioButton) findViewById(R.id.drink_yes);
         //아침 메뉴
         //메뉴 1 선택
-        Spinner b_menu1Spinner = (Spinner)findViewById(R.id.b_spinner_menu1);
-        final ArrayAdapter b_menu1Adapter = ArrayAdapter.createFromResource(this,
-                R.array.menu1, android.R.layout.simple_spinner_item);
-        b_menu1Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        b_menu1Spinner.setAdapter(b_menu1Adapter);
-
-        b_menu1Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                b_menu1 = parent.getItemAtPosition(position);
-            }
-            public void onNothingSelected(AdapterView<?>  parent) {
-            }
-        });
-
-        //메뉴 2 선택
-        Spinner b_menu2Spinner = (Spinner)findViewById(R.id.b_spinner_menu2);
-        final ArrayAdapter b_menu2Adapter = ArrayAdapter.createFromResource(this,
-                R.array.menu2, android.R.layout.simple_spinner_item);
-        b_menu2Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        b_menu2Spinner.setAdapter(b_menu2Adapter);
-
-        b_menu2Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                b_menu2 = parent.getItemAtPosition(position);
-            }
-            public void onNothingSelected(AdapterView<?>  parent) {
-            }
-        });
-
-        //메뉴 3 선택
-        Spinner b_menu3Spinner = (Spinner)findViewById(R.id.b_spinner_menu3);
-        final ArrayAdapter b_menu3Adapter = ArrayAdapter.createFromResource(this,
-                R.array.menu3, android.R.layout.simple_spinner_item);
-        b_menu3Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        b_menu3Spinner.setAdapter(b_menu3Adapter);
-
-        b_menu3Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                b_menu3 = parent.getItemAtPosition(position);
-            }
-            public void onNothingSelected(AdapterView<?>  parent) {
-            }
-        });
+//        Spinner b_menu1Spinner = (Spinner)findViewById(R.id.b_spinner_menu1);
+//        final ArrayAdapter b_menu1Adapter = ArrayAdapter.createFromResource(this,
+//                R.array.menu1, android.R.layout.simple_spinner_item);
+//        b_menu1Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        b_menu1Spinner.setAdapter(b_menu1Adapter);
+//
+//        b_menu1Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                b_menu1 = parent.getItemAtPosition(position);
+//            }
+//            public void onNothingSelected(AdapterView<?>  parent) {
+//            }
+//        });
+//
+//        //메뉴 2 선택
+//        Spinner b_menu2Spinner = (Spinner)findViewById(R.id.b_spinner_menu2);
+//        final ArrayAdapter b_menu2Adapter = ArrayAdapter.createFromResource(this,
+//                R.array.menu2, android.R.layout.simple_spinner_item);
+//        b_menu2Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        b_menu2Spinner.setAdapter(b_menu2Adapter);
+//
+//        b_menu2Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                b_menu2 = parent.getItemAtPosition(position);
+//            }
+//            public void onNothingSelected(AdapterView<?>  parent) {
+//            }
+//        });
+//
+//        //메뉴 3 선택
+//        Spinner b_menu3Spinner = (Spinner)findViewById(R.id.b_spinner_menu3);
+//        final ArrayAdapter b_menu3Adapter = ArrayAdapter.createFromResource(this,
+//                R.array.menu3, android.R.layout.simple_spinner_item);
+//        b_menu3Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        b_menu3Spinner.setAdapter(b_menu3Adapter);
+//
+//        b_menu3Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                b_menu3 = parent.getItemAtPosition(position);
+//            }
+//            public void onNothingSelected(AdapterView<?>  parent) {
+//            }
+//        });
 
         //점심 메뉴
         //메뉴 1 선택
@@ -282,5 +303,24 @@ public class WriteDiaryActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode ==1001 &&resultCode == RESULT_OK) {
+            String result = data.getExtras().getString("result_speach");
+            audioToText.setText(result);
+        }
+    }
+//    public void onClick(View v) {
+//        int view = v.getId();
+//        if(view == 2131034112) {
+//            Intent i = new Intent("android.speech.action.RECOGNIZE_SPEECH");
+//            i.putExtra("calling_package", this.getPackageName());
+//            i.putExtra("android.speech.extra.LANGUAGE", "ko-KR");
+//            i.putExtra("android.speech.extra.PROMPT", "말을 하세요.");
+//            this.startActivityForResult(i, 1000);
+//        } else if(view == 2131034113) {
+//            this.startActivityForResult(new Intent(this, CustomUIActivity.class), 1001);
+//        }
+
 
 }
