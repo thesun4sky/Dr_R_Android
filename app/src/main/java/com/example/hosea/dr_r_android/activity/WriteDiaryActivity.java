3f9ee5142a252ce72a7617ff5457eb7d5162f1ee
mslee1002 <leemsmail@naver.com>
2016-12-14 01:50:05 +0900

    일지작성 수정

--- app/src/main/java/com/example/hosea/dr_r_android/activity/WriteDiaryActivity.java
+++ app/src/main/java/com/example/hosea/dr_r_android/activity/WriteDiaryActivity.java
@@ -1,5 +1,7 @@
 package com.example.hosea.dr_r_android.activity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,12 +9,14 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -28,284 +32,143 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WriteDiaryActivity extends AppCompatActivity  {
-    private static final String BREAKFAST_SPEACH = "아침 뭐 드셨나요";
-    private static final String LUNCH_SPEACH = "점심 뭐 드셨나요";
-    private static final String DINNER_SPEACH = "저녁 뭐 드셨나요";
-    private static final int BREAKFAST_CODE =1001;
-    private static final int LUNCH_CODE =1002;
-    private static final int DINNER_CODE =1003;
-    int selected ;
-    private Intent previousIntent;
-    private AQuery aq = new AQuery(this);
-    RadioButton drinking;
-    Button submit;
-    Button recordAudio;
-    TextView breakfast;
-    TextView lunch;
-    TextView dinner;
-    Object temperature ,humidity ,sleepTime, bloodPressure ;
-    SpeechRecognizer mRecognizer;
+
+    int year, month, day;
+    TextView tv;
+    TextView today;
+    int start_year =0 , start_month=0, start_day =0;
+    TextView start;
+    TextView end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writediary);
+        GregorianCalendar calendar = new GregorianCalendar();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        tv = (TextView)findViewById(R.id.date) ;
+        today = (TextView)findViewById(R.id.today);
 
+        Spinner spinner = (Spinner)findViewById(R.id.b_spinner1);
+        final ArrayAdapter spinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.hospital, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
 
-        previousIntent = getIntent();
-        recordAudio = (Button)findViewById(R.id.audio);
-        breakfast = (TextView)findViewById(R.id.breakfast);
-        lunch = (TextView)findViewById(R.id.lunch);
-        dinner = (TextView)findViewById(R.id.dinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-
-        recordAudio.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent= new Intent(getApplicationContext(),VoiceToTextActivity.class);
-                intent.putExtra("tospeach", BREAKFAST_SPEACH);
-                startActivityForResult(intent,BREAKFAST_CODE);
+            }
+            public void onNothingSelected(AdapterView<?>  parent) {
             }
         });
 
+        findViewById(R.id.date).setOnClickListener(new View.OnClickListener() {
 
-        drinking = (RadioButton) findViewById(R.id.drink_yes);
-        //아침 메뉴
-        //메뉴 1 선택
-//        Spinner b_menu1Spinner = (Spinner)findViewById(R.id.b_spinner_menu1);
-//        final ArrayAdapter b_menu1Adapter = ArrayAdapter.createFromResource(this,
-//                R.array.menu1, android.R.layout.simple_spinner_item);
-//        b_menu1Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        b_menu1Spinner.setAdapter(b_menu1Adapter);
-//
-//        b_menu1Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                b_menu1 = parent.getItemAtPosition(position);
-//            }
-//            public void onNothingSelected(AdapterView<?>  parent) {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                DatePickerDialog datePickerDialog1 = new DatePickerDialog(WriteDiaryActivity.this, dateSetListener, year, month, day);
+                datePickerDialog1.show();
+            }
+        });
+
+        findViewById(R.id.today).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                DatePickerDialog datePickerDialog2 = new DatePickerDialog(WriteDiaryActivity.this, dateSetListener2, year, month, day);
+                datePickerDialog2.show();
+            }
+        });
+        today.setText(year+"년 "+(month+1)+"월 "+day+"일 "+ getDayKor() );
+
+
+//    public void writeDiary() {
+//        MyApplication myapp = (MyApplication)getApplicationContext();
+//        Map<String, Object> params = new HashMap<String, Object>();
+//        params.put("u_id", previousIntent.getIntExtra("u_id", 0));
+//        params.put("breakfast", breakfast.getText().toString());
+//        params.put("lunch", lunch.getText().toString());
+//        params.put("dinner",dinner.getText().toString());
+//        params.put("temperature", Integer.parseInt(temperature.toString()));
+//        params.put("humid", Integer.parseInt((humidity.toString())));
+//        params.put("sleepTime", Integer.parseInt((sleepTime.toString())));
+//        params.put("bloodPressure", Integer.parseInt((bloodPressure.toString())));
+//        if (drinking.isChecked()) {
+//            params.put("drinking", 1);
+//        } else {
+//            params.put("drinking", 0);
+//        }
+//        aq.ajax("http://52.41.218.18:8080/writeDiary", params, JSONObject.class, new AjaxCallback<JSONObject>() {
+//            @Override
+//            public void callback(String url, JSONObject html, AjaxStatus status) {
+//                try {
+//                    if (html.getString("msg").equals("정상 작동")) {
+//                        Toast.makeText(getApplicationContext(), "작성 되었습니다.", Toast.LENGTH_SHORT).show();
+//                        finish();
+//                    }
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
 //            }
 //        });
-//
-//        //메뉴 2 선택
-//        Spinner b_menu2Spinner = (Spinner)findViewById(R.id.b_spinner_menu2);
-//        final ArrayAdapter b_menu2Adapter = ArrayAdapter.createFromResource(this,
-//                R.array.menu2, android.R.layout.simple_spinner_item);
-//        b_menu2Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        b_menu2Spinner.setAdapter(b_menu2Adapter);
-//
-//        b_menu2Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                b_menu2 = parent.getItemAtPosition(position);
-//            }
-//            public void onNothingSelected(AdapterView<?>  parent) {
-//            }
-//        });
-//
-//        //메뉴 3 선택
-//        Spinner b_menu3Spinner = (Spinner)findViewById(R.id.b_spinner_menu3);
-//        final ArrayAdapter b_menu3Adapter = ArrayAdapter.createFromResource(this,
-//                R.array.menu3, android.R.layout.simple_spinner_item);
-//        b_menu3Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        b_menu3Spinner.setAdapter(b_menu3Adapter);
-//
-//        b_menu3Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                b_menu3 = parent.getItemAtPosition(position);
-//            }
-//            public void onNothingSelected(AdapterView<?>  parent) {
-//            }
-//        });
-
-
-        //온도 선택
-        Spinner tempSpinner = (Spinner)findViewById(R.id.spinner_temperature);
-        final ArrayAdapter tempAdapter = ArrayAdapter.createFromResource(this,
-                R.array.temp, android.R.layout.simple_spinner_item);
-        tempAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        tempSpinner.setAdapter(tempAdapter);
-
-        tempSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                temperature = parent.getItemAtPosition(position);
-            }
-            public void onNothingSelected(AdapterView<?>  parent) {
-            }
-        });
-
-        //습도 선택
-        Spinner humiditySpinner = (Spinner)findViewById(R.id.spinner_humidity);
-        final ArrayAdapter humidityAdapter = ArrayAdapter.createFromResource(this,
-                R.array.humidity, android.R.layout.simple_spinner_item);
-        humidityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        humiditySpinner.setAdapter(humidityAdapter);
-
-        humiditySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                humidity = parent.getItemAtPosition(position);
-            }
-            public void onNothingSelected(AdapterView<?>  parent) {
-            }
-        });
-
-        //수면시간 선택
-        Spinner sleepSpinner = (Spinner)findViewById(R.id.spinner_sleep);
-        final ArrayAdapter sleepAdapter = ArrayAdapter.createFromResource(this,
-                R.array.sleep, android.R.layout.simple_spinner_item);
-        sleepAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sleepSpinner.setAdapter(sleepAdapter);
-
-        sleepSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                sleepTime = parent.getItemAtPosition(position);
-            }
-            public void onNothingSelected(AdapterView<?>  parent) {
-            }
-        });
-
-        //혈압 선택
-        Spinner pressureSpinner = (Spinner)findViewById(R.id.spinner_pressure);
-        final ArrayAdapter pressureAdapter = ArrayAdapter.createFromResource(this,
-                R.array.pressure, android.R.layout.simple_spinner_item);
-        pressureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        pressureSpinner.setAdapter(pressureAdapter);
-
-        pressureSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                bloodPressure = parent.getItemAtPosition(position);
-            }
-            public void onNothingSelected(AdapterView<?>  parent) {
-            }
-        });
-
-        submit = (Button) findViewById(R.id.submit);
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                writeDiary();
-            }
-        });
-
     }
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
 
-    public void writeDiary() {
-        MyApplication myapp = (MyApplication)getApplicationContext();
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("u_id", previousIntent.getIntExtra("u_id", 0));
-        params.put("breakfast", breakfast.getText().toString());
-        params.put("lunch", lunch.getText().toString());
-        params.put("dinner",dinner.getText().toString());
-        params.put("temperature", Integer.parseInt(temperature.toString()));
-        params.put("humid", Integer.parseInt((humidity.toString())));
-        params.put("sleepTime", Integer.parseInt((sleepTime.toString())));
-        params.put("bloodPressure", Integer.parseInt((bloodPressure.toString())));
-        if (drinking.isChecked()) {
-            params.put("drinking", 1);
-        } else {
-            params.put("drinking", 0);
-        }
-        aq.ajax("http://52.41.218.18:8080/writeDiary", params, JSONObject.class, new AjaxCallback<JSONObject>() {
-            @Override
-            public void callback(String url, JSONObject html, AjaxStatus status) {
-                try {
-                    if (html.getString("msg").equals("정상 작동")) {
-                        Toast.makeText(getApplicationContext(), "작성 되었습니다.", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(requestCode ==BREAKFAST_CODE &&resultCode == RESULT_OK) {
-            ArrayList<String> result0 = data.getStringArrayListExtra("result_speach");
-            String[] result = new String[3];
-            result[0] = result0.get(0);
-            result[1] = result0.get(1);
-            result[2] = result0.get(2);
-            DialogSelectOption(requestCode, result);
-//            //String result = data.getExtras().getString("result_speach");
-//            Intent intent= new Intent(getApplicationContext(),VoiceToTextActivity.class);
-//            intent.putExtra("tospeach", "점심 뭐 드셧나요");
-//            startActivityForResult(intent,LUNCH_CODE);
-        }
-        if(requestCode ==LUNCH_CODE &&resultCode == RESULT_OK) {
-            ArrayList<String> result0 = data.getStringArrayListExtra("result_speach");
-            String[] result = new String[3];
-            result[0] = result0.get(0);
-            result[1] = result0.get(1);
-            result[2] = result0.get(2);
-            DialogSelectOption(requestCode, result);
-        }
-        if(requestCode ==DINNER_CODE &&resultCode == RESULT_OK) {
-            ArrayList<String> result0 = data.getStringArrayListExtra("result_speach");
-            String[] result = new String[3];
-            result[0] = result0.get(0);
-            result[1] = result0.get(1);
-            result[2] = result0.get(2);
-            DialogSelectOption(requestCode, result);
-        }
-    }
-    private void DialogSelectOption(final int requestCode, String[] item) {
-        final String[] items = item;
-        selected = 0;
-        AlertDialog.Builder ab = new AlertDialog.Builder(WriteDiaryActivity.this);
-        ab.setTitle("Title");
-        ab.setSingleChoiceItems(item, 0,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        Toast.makeText(getApplicationContext(),items[whichButton],Toast.LENGTH_SHORT).show();
-                        selected = whichButton;
-                    }
-                }).setPositiveButton("확인",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        if(requestCode == BREAKFAST_CODE) {
-                            breakfast.setText(items[selected]);
-                            Intent intent= new Intent(getApplicationContext(),VoiceToTextActivity.class);
-                            intent.putExtra("tospeach", LUNCH_SPEACH);
-                            startActivityForResult(intent,LUNCH_CODE);
-                        }
-                        if(requestCode == LUNCH_CODE){
-                            lunch.setText(items[selected]);
-                            Intent intent= new Intent(getApplicationContext(),VoiceToTextActivity.class);
-                            intent.putExtra("tospeach", DINNER_SPEACH);
-                            startActivityForResult(intent,DINNER_CODE);
-                        }
-                        if(requestCode == DINNER_CODE){
-                            dinner.setText(items[selected]);
-                        }
-                        dialog.cancel();
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            start_year = year;
+            start_month = monthOfYear +1;
+            start_day = dayOfMonth;
 
-                    }
-                }).setNegativeButton("재입력",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        dialog.cancel();
-                        if(requestCode ==BREAKFAST_CODE) {
-                            Intent intent = new Intent(getApplicationContext(), VoiceToTextActivity.class);
-                            intent.putExtra("tospeach", BREAKFAST_SPEACH);
-                            startActivityForResult(intent, requestCode);
-                        }
-                        if(requestCode ==LUNCH_CODE) {
-                            Intent intent = new Intent(getApplicationContext(), VoiceToTextActivity.class);
-                            intent.putExtra("tospeach", LUNCH_SPEACH);
-                            startActivityForResult(intent, requestCode);
-                        }
-                        if(requestCode ==DINNER_CODE) {
-                            Intent intent = new Intent(getApplicationContext(), VoiceToTextActivity.class);
-                            intent.putExtra("tospeach", DINNER_SPEACH);
-                            startActivityForResult(intent, requestCode);
-                        }
-                    }
-                });
-        ab.show();
+            tv.setText(start_year +"/"+ start_month +"/"+ start_day);
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener dateSetListener2 = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            start_year = year;
+            start_month = monthOfYear +1;
+            start_day = dayOfMonth;
+
+
+            today.setText(start_year+"년 "+(start_month)+"월 "+start_day+"일 "+ getChangeDayKor() );
+        }
+    };
+
+    public static String getDayKor(){
+        Calendar cal = Calendar.getInstance();
+        int cnt = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        String[] week = { "일", "월", "화", "수", "목", "금", "토" };
+
+        return "( "+week[cnt]+" )";
+    }
+    public String getChangeDayKor(){
+        Calendar cal= Calendar.getInstance ();
+        cal.set(Calendar.YEAR, start_year);
+        cal.set(Calendar.MONTH, start_month-1);
+        cal.set(Calendar.DATE, start_day);
+        int cnt = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        String[] week = { "일", "월", "화", "수", "목", "금", "토" };
+
+        return "( "+week[cnt]+" )";
     }
 
 }
