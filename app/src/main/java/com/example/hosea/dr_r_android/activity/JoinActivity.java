bbbbcf311cf7533488825958b862f83f2fa6f4a5
mslee1002 <leemsmail@naver.com>
2017-01-10 22:24:23 +0900

    회원가입 출생일 추가

--- app/src/main/java/com/example/hosea/dr_r_android/activity/JoinActivity.java
+++ app/src/main/java/com/example/hosea/dr_r_android/activity/JoinActivity.java
@@ -2,6 +2,7 @@ package com.example.hosea.dr_r_android.activity;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,11 +14,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidquery.AQuery;
@@ -30,8 +33,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 
@@ -42,10 +51,14 @@ public class JoinActivity extends AppCompatActivity {
     private EditText a_week, a_date;
     private EditText b_month, b_date;
     private EditText b_weight, b_height;
+    int year = 0, month = 0, day = 0;
+    int result_year = 0, result_month = 0, result_day = 0;
+    private TextView u_born;
     private static final int MY_READ_PHONE_STATE = 0;
     private Button checkId, submit;
     private String array, deviceId;
     private String[] string;
+    final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
 
 
     @Override
@@ -53,26 +66,31 @@ public class JoinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
 
+        GregorianCalendar calendar = new GregorianCalendar();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
         try {
             getUUID();
         } catch (Exception e) {
             checkPermission();
         }
 
-        a_week = (EditText)findViewById(R.id.a_week);
-        a_date = (EditText)findViewById(R.id.a_date);
-        b_month = (EditText)findViewById(R.id.b_month);
-        b_date = (EditText)findViewById(R.id.b_date);
-        b_weight =(EditText)findViewById(R.id.b_weight);
-        b_height =(EditText)findViewById(R.id.b_height);
+        a_week = (EditText) findViewById(R.id.a_week);
+        a_date = (EditText) findViewById(R.id.a_date);
+        b_month = (EditText) findViewById(R.id.b_month);
+        b_date = (EditText) findViewById(R.id.b_date);
+        b_weight = (EditText) findViewById(R.id.b_weight);
+        b_height = (EditText) findViewById(R.id.b_height);
         login_id = (EditText) findViewById(R.id.login_id);
         name = (EditText) findViewById(R.id.user_name);
+        u_born = (TextView) findViewById(R.id.u_born);
         password1 = (EditText) findViewById(R.id.password1);
         password2 = (EditText) findViewById(R.id.password2);
         checkId = (Button) findViewById(R.id.checkId);
         submit = (Button) findViewById(R.id.joinSubmit);
 
-        final RadioGroup rg = (RadioGroup)findViewById(R.id.radioForSex);
+        final RadioGroup rg = (RadioGroup) findViewById(R.id.radioForSex);
 
 
         checkId.setOnClickListener(new View.OnClickListener() {
@@ -93,12 +111,28 @@ public class JoinActivity extends AppCompatActivity {
             }
         });
 
+        u_born.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // TODO Auto-generated method stub
+                DatePickerDialog datePickerDialog1 = new DatePickerDialog(JoinActivity.this, dateSetListener, year, month, day);
+                datePickerDialog1.show();
+            }
+        });
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int id = rg.getCheckedRadioButtonId();
                 RadioButton rb = (RadioButton) findViewById(id);
-
+                String born_string = result_year + "-" + result_month + "-" + result_day + " " + "00:00:00";
+                Date born_date = null;
+                try {
+                    born_date = dateFormat.parse(born_string);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
                 //비밀번호 테스트
                 if (!password1.getText().toString().equals(password2.getText().toString()) || password1.getText().toString().length() < 5) {
                     Toast.makeText(getApplicationContext(), "비밀번호를 입력하세요. (5자 이상)", Toast.LENGTH_SHORT).show();
@@ -122,11 +156,12 @@ public class JoinActivity extends AppCompatActivity {
                     params.put("u_a_date", a_date.getText().toString());
                     params.put("u_b_month", b_month.getText().toString());
                     params.put("u_b_date", b_date.getText().toString());
+                    params.put("u_born", dateFormat.format(born_date));
                     params.put("u_w", b_weight.getText().toString());
                     params.put("u_h", b_height.getText().toString());
                     params.put("u_sex", rb.getText().toString());
-//                    params.put("u_device", deviceId);
-                    params.put("u_device", "1234");
+                    params.put("u_device", deviceId);
+
                     aq.ajax("http://52.41.218.18:8080/joinUser", params, JSONObject.class, new AjaxCallback<JSONObject>() {
                         @Override
                         public void callback(String url, JSONObject html, AjaxStatus status) {
@@ -137,7 +172,7 @@ public class JoinActivity extends AppCompatActivity {
                             }
                             Intent intent = new Intent(JoinActivity.this, LoginActivity.class);
                             {
-                                intent.putExtra("u_device" ,deviceId);
+                                intent.putExtra("u_device", deviceId);
                                 startActivity(intent);
                                 JoinActivity.this.finish();
                             }
@@ -160,6 +195,22 @@ public class JoinActivity extends AppCompatActivity {
         deviceId = deviceUuid.toString();
 
     }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+
+            result_year = year;
+            result_month = monthOfYear+1;
+            result_day = dayOfMonth;
+
+            u_born.setText(result_year+"년 "+ result_month+"월 "+result_day+"일");
+
+        }
+    };
 
 
     @TargetApi(Build.VERSION_CODES.M)
