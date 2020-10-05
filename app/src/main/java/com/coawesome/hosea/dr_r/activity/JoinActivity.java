f2e66b416c3344ce3a2317d64c6624ce0edf931e
Thesun <thesun4sky@gmail.com>
2020-10-05 22:50:15 +0900

    Modify(BugFix Lint) - Fix Lint Permission BugFix

--- app/src/main/java/com/coawesome/hosea/dr_r/activity/JoinActivity.java
+++ app/src/main/java/com/coawesome/hosea/dr_r/activity/JoinActivity.java
@@ -253,8 +253,8 @@ public class JoinActivity extends AppCompatActivity {
         final TelephonyManager tm = (TelephonyManager) getBaseContext().getSystemService(Context.TELEPHONY_SERVICE);
 
         final String tmDevice, tmSerial, androidId;
-        tmDevice = "" + tm.getDeviceId();
-        tmSerial = "" + tm.getSimSerialNumber();
+        tmDevice = "";// + tm.getDeviceId();
+        tmSerial = "";// + tm.getSimSerialNumber();
         androidId = "" + android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
 
         UUID deviceUuid = new UUID(androidId.hashCode(), ((long) tmDevice.hashCode() << 32) | tmSerial.hashCode());
