93ebec48a35f09a01d7ff289f5a5434d33f2f41c
Thesun <thesun4sky@gmail.com>
2020-10-20 00:35:49 +0900

    Add(AWS Lambda) - Add AWS Lambda dev Setting by amplify for AWS Explorer

---
+++ amplify/backend/function/diaryLambda/src/main/java/example/ResponseClass.java
@@ -0,0 +1,23 @@
+/* Amplify Params - DO NOT EDIT
+	AUTH_DRRB272D830_USERPOOLID
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+package example;
+        
+     public class ResponseClass {
+        String greetings;
+
+        public String getGreetings() {
+            return this.greetings;
+        }
+
+        public void setGreetings(String greetings) {
+            this.greetings = greetings;
+        }
+
+        public ResponseClass(String greetings) {
+            this.greetings = greetings;
+        }
+    }
